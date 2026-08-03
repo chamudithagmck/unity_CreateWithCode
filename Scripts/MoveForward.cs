fe42b2aa2d912887168bf9653ee3bc1a04029066
G.M.Chamuditha Karunarathna <chamudithakarunarathna06@gmail.com>
2026-08-03 12:30:46 +0530

    Basic Game play files added successfully

---
+++ Scripts/MoveForward.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+
+    public float speed = 35;
+
+
+
+    void Start()
+    {
+        
+    }//Strat
+
+ 
+
+
+
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+
+
+
+
+
+        
+    }//Update
+
+
+
+
+}//Class
---
+++ Scripts/SpawnManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrafabs;
+
+    private float spawnRangeX = 20;
+    private float spawnPositionZ = 30;
+
+    private float startDelay = 1;
+    private float spawnInterval = 0.5f;
+    void Start()
+    {
+
+        InvokeRepeating("SpawnRandomAnimal", startDelay , spawnInterval);
+        
+    }//Start
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
+        }
+       
+       
+
+    }//Update
+
+    void SpawnRandomAnimal()
+    {
+
+        int animalIndex = Random.Range(0, animalPrafabs.Length);
+
+
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+
+
+        Instantiate(animalPrafabs[animalIndex], spawnPos, animalPrafabs[animalIndex].transform.rotation);
+
+    }
+
+
+
+
+
+}//Class
