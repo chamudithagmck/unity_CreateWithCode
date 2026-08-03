a0494156a5354db70609409bf6ace5f56e0a53d9
G.M.Chamuditha Karunarathna <chamudithakarunarathna06@gmail.com>
2026-08-03 12:37:40 +0530

    Successfully added Sound and Effect module

---
+++ Scripts/MoveLeft.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 10;
+
+    private PlayerController _playerControllerScript;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        _playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+
+        if(_playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if(transform.position.x < -10 && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(this.gameObject);
+        }
+           
+
+
+
+        
+    }
+}
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,60 +4,83 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10;
-    public float horizontalInput;
+    private Rigidbody _rBody;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
 
-    public GameObject projectilePrafabs;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
 
 
+    public float jumpFroce = 10f;
+    public float gravityModifier;
 
-    public float xRange = 20;
 
-
-
+    public bool isOnGround = true;
+    public bool gameOver;
     void Start()
     {
-        
-    }//Start
+        _rBody = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+
+        Physics.gravity = Physics.gravity * gravityModifier;
+    }
+
+    private void OnMouseDown()
+    {
+        isOnGround = false;
+        isOnGround = false;
+        playerAnim.SetTrigger("Jump_trig");
+        dirtParticle.Stop();
+
+        playerAudio.PlayOneShot(jumpSound, 1.0f);
 
 
-
-
-
-
+        _rBody.AddForce(Vector3.up * jumpFroce, ForceMode.Impulse);
+    }
     void Update()
-    { 
-
-        // Left & Right Bounderies
-        if (transform.position.x < -xRange)
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && gameOver == false)
         {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            _rBody.AddForce(Vector3.up * jumpFroce, ForceMode.Impulse);
 
-        }  
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
 
-        if(transform.position.x > xRange)
-        {
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
 
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
-
-        //Player Inputs
-        horizontalInput = Input.GetAxis("Horizontal");
-        //Player Movements
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //Launch a projectile From the Player
-            Instantiate(projectilePrafabs, transform.position, projectilePrafabs.transform.rotation);
 
         }
 
 
 
+    }
 
+    private void OnCollisionEnter(Collision target)
+    {
+        if (target.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirtParticle.Play();
+        }
 
-    }//Update
+        else if (target.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log(" Game Over !");
+            gameOver = true;
 
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+
+            explosionParticle.Play();
+            dirtParticle.Stop();
+
+            playerAudio.PlayOneShot(crashSound, 3.0f);
+        }
+    }
 
 }//Class
